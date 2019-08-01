68d916ac1028115aad733b77e377d193ceea3634
Sidney Andrews <sidney@seesharprun.net>
2019-08-01 11:00:25 -0400

    Initial commit

---
+++ templates/sharppad3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using SharpPad;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        await "Hello World".Dump();
+    }
+}
